5822bcda9188e5bc1892bcea332617e160477d3e
unknown <hogasandreimarius@gmail.com>
2020-01-30 10:35:59 +0100

    Validation For SignUp Added

--- quackFunctions/functions/index.js
+++ quackFunctions/functions/index.js
@@ -65,6 +65,17 @@ app.post('/quack/new',(req,res) => {
     });
 });
 
+//Validation Helper Functions
+const isEmpty = (string) => {
+    if(string.trim() === '') return true;
+    else return false;
+}
+const isEmail = (email) => {
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(email.match(regEx)) return true;
+    else return false;
+}
+
 //Signup Route
 app.post('/signup', (req,res) => {
     const newUser = {
@@ -73,7 +84,28 @@ app.post('/signup', (req,res) => {
         confirmPassword: req.body.confirmPassword,
         nickname: req.body.nickname
     }
-    //Validation 
+    let errors = {};
+
+    //Validation
+    if(isEmpty(newUser.email)){
+        errors.email = 'Email must not be emtpy';
+    } else if (!isEmail(newUser.email)) {
+        errors.email = 'You must fill in a valid email address';
+    }
+    if(isEmpty(newUser.password)){
+        errors.password = "You must fill in your password";
+    }
+    if(newUser.password !== newUser.confirmPassword){
+        errors.confirmPassword = "Passwords did not match";
+    }
+    if(isEmpty(newUser.nickname)){
+        errors.nickname = "You must choose a nickname";
+    }
+    if(Object.keys(errors).length > 0){
+        return res.status(400).json({errors});
+    }
+    
+    
     let token,userId;
     db.doc(`/users/${newUser.nickname}`)
     .get()
