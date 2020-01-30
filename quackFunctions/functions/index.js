bfc9743b0dab6d8b339371afa652012f5343a0fc
unknown <hogasandreimarius@gmail.com>
2020-01-30 09:47:54 +0100

    Basic Auth Route Added

--- quackFunctions/functions/index.js
+++ quackFunctions/functions/index.js
@@ -1,13 +1,30 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
+
 admin.initializeApp();
 
 //Express init
 const express = require('express');
 const app = express();
 
+//Firebase init
+const firebaseConfig = {
+    apiKey: "AIzaSyDXCyhqlGAulAgvn3be1u-0paWK1SD3QHk",
+    authDomain: "quack-1d8e3.firebaseapp.com",
+    databaseURL: "https://quack-1d8e3.firebaseio.com",
+    projectId: "quack-1d8e3",
+    storageBucket: "quack-1d8e3.appspot.com",
+    messagingSenderId: "172044958007",
+    appId: "1:172044958007:web:eab3ad688b212e990cc2b2",
+    measurementId: "G-B3H9FLCL9Z"
+};
 
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
+
+
+//Get all Quacks Route
 app.get('/quacks',(req, res) =>{
     admin
     .firestore()
@@ -29,6 +46,7 @@ app.get('/quacks',(req, res) =>{
     .catch(err => console.error(err)); 
 })
 
+//Create new quack route
 app.post('/quack/new',(req,res) => {
     const newQuack = {
         body: req.body.body,
@@ -46,6 +64,26 @@ app.post('/quack/new',(req,res) => {
         console.error(err);
     });
 });
+
+//Signup Route
+app.post('/signup', (req,res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        nickname: req.body.nickname
+    }
+    //TODO: Validate data
+    firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then(data => {
+        return res.status(201).json({message: `user ${data.user.uid} signed up succesfully`});
+        
+    })
+    .catch(err => {
+        console.error(err);
+        return res.status(500).json({ error: err.code});
+    })
+})
 
 
 
